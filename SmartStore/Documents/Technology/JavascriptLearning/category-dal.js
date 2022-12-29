269c35916aeff4d4d98d8e26810311f1313d3011
PriyankaJena952001 <119510351+PriyankaJena952001@users.noreply.github.com>
2022-12-29 13:36:30 +0530

    Add files via upload

---
+++ SmartStore/Documents/Technology/JavascriptLearning/category-dal.js
@@ -0,0 +1,23 @@
+const mongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017';
+const db_name = 'ss';
+const collection_name = 'Category';
+
+function add(){
+    console.log('inserting');
+    const client = new mongoClient(url);
+    const db = client.db(db_name);
+    const collection = db.collection(collection_name);
+    client.connect();
+
+    collection.insertOne({
+     category_id: 1,
+     category_name: 'Cola'
+    });
+    console.log('inserted');
+}
+ 
+module.exports = {
+  add: add
+};
